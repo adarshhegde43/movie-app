a91ed291f68ceb16c1cfdead0d51c86e3acf7ded
Adarsh Hegde <ch22b048@smail.iitm.ac.in>
2025-03-02 14:47:34 +0530

    Modified several files for improvements

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,23 +1,29 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 const MovieList = (props) => {
-	const FavouriteComponent = props.favouriteComponent;
+  const FavouriteComponent = props.favouriteComponent;
 
-	return (
-		<>
-			{props.movies.map((movie, index) => (
-				<div className='image-container d-flex justify-content-start m-3'>
-					<img src={movie.Poster} alt='movie'></img>
-					<div
-						onClick={() => props.handleFavouritesClick(movie)}
-						className='overlay d-flex align-items-center justify-content-center'
-					>
-						<FavouriteComponent />
-					</div>
-				</div>
-			))}
-		</>
-	);
+  return (
+    <>
+      {props.movies.map((movie, index) => (
+        <motion.div
+          className='image-container d-flex justify-content-start m-3'
+          key={movie.imdbID}
+          whileHover={{ scale: 1.1 }}
+          transition={{ type: "spring", stiffness: 300 }}
+        >
+          <img src={movie.Poster} alt='movie'></img>
+          <div
+            onClick={() => props.handleFavouritesClick(movie)}
+            className='overlay d-flex align-items-center justify-content-center'
+          >
+            <FavouriteComponent />
+          </div>
+        </motion.div>
+      ))}
+    </>
+  );
 };
 
 export default MovieList;
